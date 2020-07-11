f28da19115daeccda8600ed1c8bd57d19fb36b05
niks.janeks@gmail.com <niks.janeks@gmail.com>
2020-07-11 16:28:34 +0300

    version mid

--- Game.cs
+++ Game.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,26 @@ namespace SnakeGame
 {
     public partial class Game : Form
     {
+        Area area = new Area();
+        Snake snake = new Snake();
         public Game()
         {
             InitializeComponent();
+            InitializeGameArea();
+        }
+
+        private void InitializeGameArea()
+        {
+            this.Height = 600;
+            this.Width = 600;
+
+            this.Controls.Add(area);
+            //this.MaximumSize = area.Size;
+            //this.MinimumSize = area.Size;
+            area.Top = 100;
+            area.Left = 100;
+
+            snake.Render(this);
         }
     }
 }
